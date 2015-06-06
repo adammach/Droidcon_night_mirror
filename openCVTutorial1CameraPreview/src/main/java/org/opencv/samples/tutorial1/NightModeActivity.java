617c4c59498059f6340d1492748967b4dd4f8db5
Adam Mach <adam.mach@currency-one.com>
2015-06-06 13:12:52 +0200

    last commit 2

--- openCVTutorial1CameraPreview/src/main/java/org/opencv/samples/tutorial1/NightModeActivity.java
+++ openCVTutorial1CameraPreview/src/main/java/org/opencv/samples/tutorial1/NightModeActivity.java
@@ -129,43 +129,9 @@ public class NightModeActivity extends Activity implements CvCameraViewListener2
         hierarchy = new Mat();
         Imgproc.Canny(mGray, mIntermediateMat, 80, 100);
         Imgproc.findContours(mIntermediateMat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        //TODO show road lanes in differnt color.
        // Imgproc.HoughLines(mIntermediateMat, mLines, 1, Math.PI / 180, 20);
-//        for (int x = 0; x < mLines.cols(); x++)
-//        {
-////            double[] vec = mLines.get(0, x);
-////            double x1 = vec[0],
-////                    y1 = vec[1],
-////                    x2 = vec[2],
-////                    y2 = vec[3];
-////            Point start = new Point(x1, y1);
-////            Point end = new Point(x2, y2);
-////
-////            Imgproc.line(mRgba, start, end, new Scalar(255, 123, 0), 3);
-////            a = np.cos(theta)
-////            b = np.sin(theta)
-////            x0 = a*rho
-////            y0 = b*rho
-////            x1 = int(x0 + 1000*(-b))
-////            y1 = int(y0 + 1000*(a))
-////            x2 = int(x0 - 1000*(-b))
-////            y2 = int(y0 - 1000*(a))
-////
-////            cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-//
-//        }
-        //mIntermediateMat.release();
 
-//        contours = new ArrayList<MatOfPoint>();
-//        hierarchy = new Mat();
-//
-//        Imgproc.Canny(mRgba, mIntermediateMat, 80, 100);
-//        Imgproc.findContours(mIntermediateMat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
-//    /* Mat drawing = Mat.zeros( mIntermediateMat.size(), CvType.CV_8UC3 );
-//     for( int i = 0; i< contours.size(); i++ )
-//     {
-//    Scalar color =new Scalar(Math.random()*255, Math.random()*255, Math.random()*255);
-//     Imgproc.drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, new Point() );
-//     }*/
 
         Imgproc.drawContours(mRgba, contours, -1, new Scalar(255, 0, 0));//, 2, 8, hierarchy, 0, new Point());
 //        // Imgproc.cvtColor(mIntermediateMat, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
